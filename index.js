384f16a8d141db1557e90dad80c7b10c3ceb0cc3
G-U-T <147336533+G-U-T@users.noreply.github.com>
2023-11-09 11:25:04 -0500

    Can focus specific card.

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ let currentPlayers = [];
 
 const puppyGrid = document.getElementById(`puppy-grid`);
 
-const constructCard = (player) => {
+const constructCard = (player, isMain) => {
 	const cardBase = document.createElement(`section`);
 	cardBase.classList.add(`center-flex`);
 	puppyGrid.appendChild(cardBase);
 
 	const cardLink = document.createElement(`button`);
 	cardLink.innerText = player.name;
+	if (isMain) {
+		cardBase.classList.add(`focused-card`);
+		cardLink.addEventListener(`click`, () => {
+			main();
+			render();
+		});
+	}
+	else {
+		cardLink.addEventListener(`click`, () => {
+			renderSpecificPlayerDetails(player);
+		});
+	}
+	
 	cardBase.appendChild(cardLink);
 
 	const image = document.createElement(`img`);
@@ -17,9 +30,17 @@ const constructCard = (player) => {
 };
 
 const render = () => {
+	puppyGrid.innerHTML = ``;
+
 	currentPlayers.forEach((player) => {
-		constructCard(player);
+		constructCard(player, false);
 	});
+};
+
+const renderSpecificPlayerDetails = (player) => {
+	puppyGrid.innerHTML = ``;
+
+	constructCard(player, true);
 };
 
 const getJSON = async () => {
