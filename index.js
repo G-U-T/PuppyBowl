cdbedaf20d3239a56581d8007e60e2d0fb94c386
G-U-T <147336533+G-U-T@users.noreply.github.com>
2023-11-09 13:52:33 -0500

    Can submit puppy through form.

--- index.js
+++ index.js
@@ -2,6 +2,29 @@ let currentPlayers = [];
 
 const puppyGrid = document.getElementById(`puppy-grid`);
 
+const form = document.querySelector(`form`);
+
+const nameInput = document.querySelector(`[name='name']`);
+const imageInput = document.querySelector(`[name='image']`);
+const statusInput = document.querySelector(`[name='status']`);
+const breedInput = document.querySelector(`[name='breed']`);
+const teamInput = document.querySelector(`[name='team-id']`);
+const cohortInput = document.querySelector(`[name='cohort-id']`);
+
+form.addEventListener(`submit`, (event) => {
+	event.preventDefault();
+
+	const createdPuppy = {
+		name: nameInput.value,
+		imageUrl: imageInput.value,
+		status: statusInput.value,
+		breed: breedInput.value,
+		teamId: teamInput.value,
+		cohortId: cohortInput.value,
+	}
+	postPuppy(createdPuppy);
+});
+
 const postPuppy = async (puppy) => {
 	const pup = await fetch(`https://fsa-puppy-bowl.herokuapp.com/api/2310-fsa-et-web-ft-sf/players`,
 	{
@@ -11,11 +34,14 @@ const postPuppy = async (puppy) => {
 			imageUrl: puppy.imageUrl,
 			status: puppy.status,
 			breed: puppy.breed,
+			teamId: puppy.teamId,
+			cohortId: puppy.cohortId,
 		}),
 		headers: {
 			"Content-Type": "application/json",
 		},
 	});
+	main();
 };
 
 const constructCard = (player, isMain) => {
@@ -28,9 +54,7 @@ const constructCard = (player, isMain) => {
 	if (isMain) {
 		cardBase.classList.add(`focused-card`);
 		cardLink.addEventListener(`click`, () => {
-			postPuppy(player);
 			main();
-			render();
 		});
 	}
 	else {
@@ -44,6 +68,23 @@ const constructCard = (player, isMain) => {
 	const image = document.createElement(`img`);
 	image.src = player.imageUrl;
 	cardBase.appendChild(image);
+
+	if (isMain) {
+		const breedLabel = document.createElement(`label`);
+		breedLabel.innerText = `Breed: ` + player.breed;
+		cardBase.appendChild(breedLabel);
+
+		const statusLabel = document.createElement(`label`);
+		statusLabel.innerText = `Status: ` + player.status;
+		cardBase.appendChild(statusLabel);
+
+		const dupeButton = document.createElement(`button`);
+		dupeButton.innerText = `Duplicate Puppy`;
+		dupeButton.addEventListener(`click`, () => {
+			postPuppy(player);
+		});
+		cardBase.appendChild(dupeButton);
+	}
 };
 
 const render = () => {
